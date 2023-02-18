4284cc11a6528b62145ddfe5d489bdd928b29afd
yujisu01 <jssla1@naver.com>
2023-02-18 14:27:00 +0900

    220218

---
+++ src/main/java/com/test/cotest/co03/guganhap03.java
@@ -0,0 +1,12 @@
+package com.test.cotest.co03;
+
+import java.util.Scanner;
+
+public class guganhap03 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		
+	}
+
+}
