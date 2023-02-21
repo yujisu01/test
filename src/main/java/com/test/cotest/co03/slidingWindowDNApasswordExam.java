f50250bd45ae3a67863352df291e2b7a8578edba
yujisu01 <jssla1@naver.com>
2023-02-21 18:10:28 +0900

    230221

--- src/main/java/com/test/cotest/co03/slidingWindowDNApasswordExam.java
+++ src/main/java/com/test/cotest/co03/slidingWindowDNApasswordExam.java
@@ -32,9 +32,9 @@ public class slidingWindowDNApasswordExam {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		
-		// 전체 문자열 크기 선언
+		// 전체 문자열 크기 선언 (입력가능)
 		int S = Integer.parseInt(st.nextToken());
-		// 부분문자열 크기 선언
+		// 부분문자열 크기 선언 (입력가능)
 		int P = Integer.parseInt(st.nextToken());
 		int result= 0;
 		
@@ -42,5 +42,78 @@ public class slidingWindowDNApasswordExam {
 		checkArr = new int[4];		// 비밀번호 체크배열
 		myArr = new int[4];			// 현재상태 배열
 		checkSecret =0;				// 몇개의 문자와 관련된 개수를 충족했는지 판단하는 변수
+		
+		A = bf.readLine().toCharArray();			// A배열도 문자열도 char배열로 친절히 쪼개주기위해
+		st = new StringTokenizer(bf.readLine());	// for문 전 선언
+		
+		// DNA비밀번호 문자열 입력 (ex: CGTA..)
+		for(int i=0; i<4; i++) {
+			// 비밀번호 체크배열 파싱
+			checkArr[i] = Integer.parseInt(st.nextToken());
+			if(checkArr[i] == 0) checkSecret++;
+		}
+		// 0번째부터 P까지 일단 첫번째로 DNA패스워드 확인하고 Add
+		for(int i=0; i<P; i++) {
+			Add(A[i]);
+		}
+		// checkSecret이 ACGT 다 해당되면 result++
+		if(checkSecret==4) result++;
+		
+		//슬라이딩윈도우 처리부분
+		// 위에서 0번째부터P까지 DNA 확인했으니 이제 한칸씩 이동하면서 제거되는 문자열과, 새로 들어오는 문자열 처리
+		for(int i= P; i<S; i++) {
+			int j =i-P;
+			Add(A[i]);
+			Remove(A[j]);
+			
+			// 유효한 비밀번호인지 (checkSecret==4) 판단해 결과값에 업데이트
+			if(checkSecret==4) result++;
+		}
+		System.out.println(result);
+		bf.close();
+	}
+	
+	
+	// Add, Remove 함수 처리 부분 
+	private static void Add(char c) {
+		switch(c) {
+		case 'A':
+			myArr[0]++;
+			if(myArr[0] == checkArr[0]) checkSecret++;
+			break;
+		case 'C':
+			myArr[1]++;
+			if(myArr[1] == checkArr[1]) checkSecret++;
+			break;
+		case 'G':
+			myArr[2]++;
+			if(myArr[2] == checkArr[2]) checkSecret++;
+			break;
+		case 'T':
+			myArr[3]++;
+			if(myArr[3] == checkArr[3]) checkSecret++;
+			break;
+		
+		}
+	}
+	private static void Remove(char c) {
+		switch(c) {
+		case 'A':
+			if(myArr[0] == checkArr[0]) checkSecret--;
+			myArr[0]--;
+			break;
+		case 'C':
+			if(myArr[1] == checkArr[1]) checkSecret--;
+			myArr[1]--;
+			break;
+		case 'G':
+			if(myArr[2] == checkArr[2]) checkSecret--;
+			myArr[2]--;
+			break;
+		case 'T':
+			if(myArr[3] == checkArr[3]) checkSecret--;
+			myArr[3]--;
+			break;
+		}
 	}
 }
