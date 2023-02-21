4e681ae907d59434ef991de15247d3da4815f266
yujisu01 <jssla1@naver.com>
2023-02-21 16:46:26 +0900

    230221

--- src/main/java/com/test/cotest/co03/qiuhaoshu.java
+++ src/main/java/com/test/cotest/co03/qiuhaoshu.java
@@ -3,6 +3,7 @@ package com.test.cotest.co03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -18,6 +19,8 @@ public class qiuhaoshu {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));	// 배열 Scanner 역할
 		int N = Integer.parseInt(bf.readLine());
+		
+		// =count
 		int result =0;
 		long[] A = new long[N];
 		
@@ -30,7 +33,41 @@ public class qiuhaoshu {
 			// twoPointer02 클래스에서 Integer.parseInt로 변환하는거는 A[i]를 int 형으로 선언했기 때문 (여기선 long으로 선언) 
 			A[i]= Long.parseLong(st.nextToken());
 		}
+		// 일단 오름차순 정렬
+		Arrays.sort(A);
 		
+		for(int k=0; k<N; k++) {
+			long find= A[k];
+			int i=0;
+			int j=N-1;
+			
+			while(i<j) {
+				// 좋은수 찾는 값이 일치하는 경우에 두가지 경우의수 있음.
+				// (1) 서로 다른 두수의 합이어야 함을 체크
+				// (2) 
+				if(A[i]+A[j] == find) {
+					// 서로 다른 두수면 ok
+					if(i != k && j != k) {
+						result++;
+						break;
+					// else if 두 포인터 i,j가 k와 일치할때, 포인터 변경및 계속 수행하기..
+					// 없어도 되는거 아닝가 ??아모르겠다 누가 설명좀..
+					}else if(i==k) {
+						i++;
+					}else if(j==k) {
+						j--;
+					}
+				// 두수 합이 find보다 작을경우 작은값 i증가 
+				}else if (A[i]+A[j] < find){
+					i++;
+				// 두수 합이 find보다 클경우 큰값인 j 감소 
+				}else {
+					j--;
+				}
+			}
+		}// for문 벗어나서
+		System.out.println(result);
+		bf.close();
 				
 	}
 
---
+++ src/main/java/com/test/cotest/co03/slidingWindow.java
@@ -0,0 +1,12 @@
+package com.test.cotest.co03;
+/*
+ * 슬라이딩윈도우가 뭘까? 2개의 포인터로 범위 지정한다음 범위를 유지한채로, 이동하여 문제 해결하는거
+ * 투포인터 알고리즘과 비슷(i,j..)
+ * 
+ * DNA비밀번호 문제
+ * 
+ * 
+ */
+public class slidingWindow {
+
+}
