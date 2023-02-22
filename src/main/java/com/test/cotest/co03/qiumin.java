fa7cdc74dfaeaf1c281447fb209e234825f267ec
yujisu01 <jssla1@naver.com>
2023-02-22 16:13:37 +0900

    230222

---
+++ src/main/java/com/test/cotest/co03/qiumin.java
@@ -0,0 +1,16 @@
+package com.test.cotest.co03;
+/*
+ * 플래 ㅅ;
+ * 
+ * 최솟값 찾기..
+ * 1번째줄에 N(1<=L<=5,000,000),
+ * 2번째줄에 N개의수 A가 주어진다 (-109<=A<=109)
+ * 
+ * addFirst(), removeFirst() 함수로 삽입,삭제 역할을 한다.
+ * addLast(), removeLast() 마찬가지로 삽입,삭제 역할. 덱을 이용한 정렬효과
+ */
+public class qiumin {
+	public static void main(String[] args) {
+		
+	}
+}
