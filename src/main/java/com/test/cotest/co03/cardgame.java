4cf5d475e75a7d1d6823f0bb2d8e8c0e7acbb7ca
yujisu01 <jssla1@naver.com>
2023-03-04 00:00:51 +0900

    230304

--- src/main/java/com/test/cotest/co03/cardgame.java
+++ src/main/java/com/test/cotest/co03/cardgame.java
@@ -19,6 +19,16 @@ public class cardgame {
 		Queue<Integer> que= new LinkedList<Integer>();
 		int N= sc.nextInt();	// N만큼 입력받는다.
 		
+		// for문 돌려서 카드를 큐에 저장한다. 
+		// i는 1부터 시작해야됨. 
+		for(int i=1; i<=N; i++) {	
+			que.add(i);
+		}
+		while(que.size() > 1) {	// 카드가 하나남을떄까지
+			que.poll();			// 맨위 카드를 버리고
+			que.add(que.poll());	// 맨위 카드를 가장 아래 카드로 옮김.
+		}
+		System.out.println(que.poll());	// 마지막으로 남은 카드 출력하기
 	}
 
 }
