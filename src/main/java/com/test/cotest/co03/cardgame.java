98c277bf7bc483b5eabe95bd13de4c88f71f90f9
yujisu01 <jssla1@naver.com>
2023-03-03 23:52:46 +0900

    230303

--- src/main/java/com/test/cotest/co03/cardgame.java
+++ src/main/java/com/test/cotest/co03/cardgame.java
@@ -1,10 +1,24 @@
 package com.test.cotest.co03;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /*
- * 
+ * 카드게임
+ * Que 이해하기 
+ * 1에서 N까지의 번호가 있다. 1이 위, N이 제일아래. 
+ * 가장위에 있는 카드를 버리고, 그다음 가장위에 있는 카드를 가장 아래에 있는 카드밑으로 옮김
+ * 그래서 마지막에 남는 숫자 구하기
  */
 
 public class cardgame {
-	
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Queue<Integer> que= new LinkedList<Integer>();
+		int N= sc.nextInt();	// N만큼 입력받는다.
+		
+	}
 
 }
