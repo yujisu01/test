dd75816b2c00ed3ef9a1d49837cb10bddd23c1c4
yujisu01 <jssla1@naver.com>
2023-03-07 16:39:38 +0900

    230307

--- src/main/java/com/test/cotest/co04/sort03_desc.java
+++ src/main/java/com/test/cotest/co04/sort03_desc.java
@@ -8,25 +8,34 @@ import java.util.Scanner;
 
 public class sort03_desc {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N= sc.nextInt();
-		int[] A= new int[N];
+// (1) 내림차순 정렬 방법 
+//		Scanner sc = new Scanner(System.in);
+//		int N= sc.nextInt();
+//		int[] A= new int[N];
+//		
+//		for(int i=0; i<N; i++) {
+//			A[i] = sc.nextInt();
+//		}
+//		for(int i=0; i<N-1;i++) {
+//			for(int j=0; j<N-1; j++) {
+//				if(A[j] < A[j+1]) {
+//					int temp = A[j];
+//					A[j] = A[j+1];
+//					A[j+1] = temp;
+//				}
+//			}
+//		}
+//		for(int i=0; i<N; i++) {
+//			System.out.println(A[i]);
+//		}
 		
-		for(int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
-		}
-		for(int i=0; i<N-1;i++) {
-			for(int j=0; j<N-1; j++) {
-				if(A[j] < A[j+1]) {
-					int temp = A[j];
-					A[j] = A[j+1];
-					A[j+1] = temp;
-				}
-			}
-		}
-		for(int i=0; i<N; i++) {
-			System.out.println(A[i]);
-		}
+		
+/*
+ *  (2) 입력값을 String으로 받은후 substring() 함수를 이용해 다시 int형으로 변경해 배열에 저장하여 
+ *  	내림차순으로 정렬하는 방법
+ */
+		
+		
 	}
 	
 }
