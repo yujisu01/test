7bae20e9e719b47b25a3a72283d2145e169a9003
yujisu01 <jssla1@naver.com>
2023-03-08 21:24:39 +0900

    230308

--- src/main/java/com/test/cotest/co04/sort04_insert.java
+++ src/main/java/com/test/cotest/co04/sort04_insert.java
@@ -13,10 +13,13 @@ public class sort04_insert {
 		for(int i=0; i<N; i++) {
 			A[i]= sc.nextInt();
 		}
+		// -----여기까진 선언 & 배열 저장 
+		// -----여기부터 삽입정렬하기위해 for문 돌리기 (최솟값을 구하는 과정)
+		// 현재범위에서 삽입위치 찾기
 		for(int i=1; i<N; i++) {
+			// 삽입이 들어가는 포인트,value값 설정
 			int insert_point =i;
 			int insert_value= A[i];
-			
 			for(int j=i-1; j>=0; j--) {
 				if(A[j]<A[i]) {
 					insert_point =j+1;
@@ -26,16 +29,22 @@ public class sort04_insert {
 					insert_point=0;
 				}
 			}
+			// 삽입을 위해 삽입위치에서 i까지 데이터를 한칸씩 뒤로 밀기
 			for(int j=i; j>insert_point; j--) {
 				A[j] = A[j-1];
 			}
 			A[insert_point] = insert_value;
 		}
+		// 삽입 위치에 현재 데이터 삽입하기 
+		// 이 코드를 입력해줘야 0번인덱스부터 잘 출력이됨
 		S[0] = A[0];
 		
 		// 합배열 구하기
 		for(int i= 1; i<N; i++) {
+			// A[i] 합해야 내가 입력한거 대로 합배열 출력됨
 			S[i] = S[i-1]+A[i];
+			System.out.println(S[i]);
+			
 		}
 		// sum출력
 		int sum=0;
