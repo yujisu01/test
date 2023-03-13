292641751c4e6ad153f4fd988f1737652f42b6c4
yujisu01 <jssla1@naver.com>
2023-03-13 16:58:37 +0900

    230313

--- src/main/java/com/test/cotest/co04/sort08_radixSort.java
+++ src/main/java/com/test/cotest/co04/sort08_radixSort.java
@@ -22,7 +22,7 @@ public class sort08_radixSort {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N= Integer.parseInt(br.readLine());
-		int A[] = new int[N];
+		A = new int[N];
 		// A[i] 배열 for문 선언 
 		for(int i=0; i<N; i++) {
 			A[i] = Integer.parseInt(br.readLine());
@@ -38,14 +38,50 @@ public class sort08_radixSort {
 		// bw close
 		bw.flush();
 		bw.close();
-	}
+	} 
 	// 여기까지 main 함수
 	
 	// 여기부터 radix_sort() 실행 
 	// 일반적 기수정렬은 우선순위 큐를 사용해 비교적 간단하게 구현하는 방법이 있지만, 시간복잡도를 느리게 하므로
 	// 구간합을 이용하는 방법으로 구현
 	// bucket 배열의 정확한 의미와 bucket 배열로 현재자리를 기준으로 정렬하는 방법
+	// max_size를 5로 설정(자릿값이 0~4까지 몇개의 데이터가 있는지 저장)
 	public static void radix_sort(int[] A, int max_size) {
+		// 임시정렬 배열
+		int[] output = new int[A.length];
+		// 현재 자릿수 표현 (1의자리, 10의자리 ~..)
+		int jarisu = 1;
+		int count =0; 
+		
+		// 최대 자릿수만큼 반복 while문 시작
+		while(count != max_size) {
+			// 0~9 까지 라서 int[10] 
+			int[] bucket = new int[10];
+			
+			// 1의 자리부터 시작
+			for(int i=0; i<A.length; i++) {
+				bucket[(A[i] / jarisu) % 10]++;
+			}
+			// 합배열을 이용해 index계산 
+			// S[i] = S[i-1] + A[i] 
+			// 주석시 0번쨰 인덱스만 출력됨 
+			for(int i=1; i<10; i++) {
+				bucket[i] += bucket[i-1];
+			}
+			for(int i = A.length-1; i>=0; i--) {
+				output[bucket[(A[i] / jarisu % 10)] - 1] = A[i];
+				bucket[(A[i] / jarisu) % 10]--;
+			}
+			
+			// 다음 자릿수 이동을 위해 현재 자릿수 기준 정렬데이터 저장
+			for(int i=0; i<A.length; i++) {
+				A[i] = output[i];
+				//output[i] = A[i];
+			}
+			jarisu = jarisu*10;
+			count++;
+		}
+		
 		
 	}
 
