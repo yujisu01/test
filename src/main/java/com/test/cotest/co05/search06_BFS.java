5318dfb455b8112dc130a1bc299058de71d788c9
yujisu01 <jssla1@naver.com>
2023-03-22 19:46:00 +0900

    230322

---
+++ src/main/java/com/test/cotest/co05/search06_BFS.java
@@ -0,0 +1,10 @@
+package com.test.cotest.co05;
+/*
+ * 너비우선탐색(BFS: breadth first search) 
+ * 시작노드에서 출발해, 시작노드를 기준으로 가까운 노드를 먼저 방문하면서 탐색한다
+ * 특징: FIFO탐색, 큐 자료구조(Queue) 
+ * 선입선출 방식이여서 큐를 이용한다. 
+ */
+public class search06_BFS {
+
+}
