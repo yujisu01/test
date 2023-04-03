604b83d2c280915846313c81d12b65806159639c
yujisu01 <jssla1@naver.com>
2023-04-03 20:47:14 +0900

    230304

--- src/main/java/com/test/cotest/co06/greedy01_coinMin.java
+++ src/main/java/com/test/cotest/co06/greedy01_coinMin.java
@@ -1,4 +1,8 @@
 package com.test.cotest.co06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
  * 그리디 알고리즘: 현재 상태의 선택지중 최선의 선택을 하는 알고리즘. 
  * 수행과정
@@ -24,10 +28,35 @@ package com.test.cotest.co06;
  * 
  * 전형적인 그리디 알고리즘 문제이다. 동전을 최소로 사용하여 K를 만드려면 가장 가격이 큰 돈부터 차례대로 사용
  * 
+ * count 값을 구함
+ * 
  */
-public class greedy01_coinMin {
+public class greedy01_coinMin { 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int A[] = new int[N];
+		for(int i=0;i<N;i++) {
+			A[i]=sc.nextInt(); 
+		}
+		// 동전개수를 저장할 변수 count
+		int count =0;
+		// 일단 오름차순 정렬해줌 안하면 결과 엉뚱하게나옴
+		Arrays.sort(A);
 		
+		// 가치가 큰 동전부터 사용하기위한 내림차순 for문 
+		for(int i = N-1; i>=0; i--) {
+			// 만약 현재 동전 가치가 K보다 작거나 같으면 구성에 추가 
+			if(K >= A[i]) {
+				count = count + (K / A[i]);  
+				//System.out.printf("for문 속의 K (1)  : " + K + "\n");
+				//K = K / A[i]; 4200, 4, 8, 4 
+				K = K % A[i]; 
+				//System.out.printf("for문 속의 K (2)  : " + K + "\n");
+			}
+		}
+		System.out.printf("count : " + count + " ,K: " + K + "\n"); 
 	}
 
 }
