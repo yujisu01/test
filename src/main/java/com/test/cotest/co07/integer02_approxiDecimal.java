86ed259506be5e6619a988802dda6d6db75b3d02
yujisu01 <jssla1@naver.com>
2023-04-11 12:32:50 +0900

    230411

---
+++ src/main/java/com/test/cotest/co07/integer02_approxiDecimal.java
@@ -0,0 +1,21 @@
+package com.test.cotest.co07;
+/*
+ * 거의소수 구하기
+ * 어떤수가 소수의 N제곱 일때 이 수를 '거의소수'라 함. A,B가 있을때 A보다 크거나같고, B보다 작거나 같은 
+ * 거의소수가 몇갠지 출력
+ * 
+ * 1번쨰 줄에 왼쪽범위A와, 오른쪽범위B가 주어짐.
+ * A의 범위는 10의14제곱보다 작거나 같은 자연수, B는 A보다 크거나 같고 10의 14제곱보다 작거나 같은 자연수
+ * (A<=10의14승), (10의14승>=B>=A)
+ * 
+ * ex입력
+ * 1 1000
+ * 출력
+ * 25
+ */
+public class integer02_approxiDecimal {
+	public static void main(String[] args) {
+		
+	}
+
+}
